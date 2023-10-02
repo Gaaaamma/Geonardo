194fe8f5d2779893d04582c144e94b96ffa3a55e
Gaaaamma <ytching552022@gmail.com>
2023-10-02 19:02:42 +0800

    Add: divide long move into multiple move

--- simpleController/simpleController.ino
+++ simpleController/simpleController.ino
@@ -13,11 +13,15 @@ e(101): keyboard write 'Enter'
 
 k(107): key in a certain character (with one following ASCII code)
 */
+
+#include <Mouse.h>
 long CLICK_FREQUENCY = 100;
+long MOVE_STEP = 30;  // 0 < MOVE_STEP < 128
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  // Mouse.begin();
 }
 
 void loop() {
@@ -30,7 +34,7 @@ void loop() {
       long x = Serial.parseInt();
       Serial.read();  // consume ',' in the buffer
       long y = Serial.parseInt();
-      Move(x, y);
+      LongMove(x, y);
 
     } else if (c == 's') {
       // Serial.println("[Command] single click");
@@ -48,15 +52,46 @@ void loop() {
   }
 }
 
-void Move(long x, long y) {
+void LongMove(long x, long y) {
+  // Moving distance might over one step => divide it into multiple pieces
+  long xTimes = x / MOVE_STEP;
+  long yTimes = y / MOVE_STEP;
+
+  long xRemain = x - (xTimes * MOVE_STEP);
+  long yRemain = y - (yTimes * MOVE_STEP);
+
+  // Move X
+  long direction = (xTimes >= 0) ? 1 : -1;
+  for (long i = 0; i < abs(xTimes); i++) {
+    Move((signed char)(MOVE_STEP * direction), 0);
+  }
+  if (xRemain != 0) {
+    Move((signed char)xRemain, 0);
+  }
+
+  // Move Y
+  direction = (yTimes >= 0) ? 1 : -1;
+  for (long i = 0; i < abs(yTimes); i++) {
+    Move(0, (signed char)(MOVE_STEP * direction));
+  }
+  if (yRemain != 0) {
+    Move(0, (signed char)yRemain);
+  }
+}
+
+void Move(signed char x, signed char y) {
+  // Must use a switch or some controller to keep it safe
   Serial.print("[Mouse] moving (");
-  Serial.print(x);
+  Serial.print((int)x);
   Serial.print(",");
-  Serial.print(y);
+  Serial.print((int)y);
   Serial.println(")");
+
+  // Mouse.move(x, y);
 }
 
 void Click() {
+  // Must use a switch or some controller to keep it safe
   Serial.println("[Mouse] click");
 }
 
