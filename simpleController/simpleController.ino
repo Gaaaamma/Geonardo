14450d1981406005640b24a20b4dd3554b8e2edb
Gaaaamma <ytching552022@gmail.com>
2023-10-02 13:10:12 +0800

    Modify: simple testing controller

--- simpleController/simpleController.ino
+++ simpleController/simpleController.ino
@@ -1,3 +1,14 @@
+/*
+Command list:
+s: single click
+d: double click
+m: move mouse position (with two following integer)
+  - x: relative x moving
+  - y: relative y moving
+e: keyboard write 'Enter'
+k: key in a certain character (with one following ASCII code)
+*/
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -12,7 +23,3 @@ void loop() {
     }
   }
 }
-
-/*
-
-*/
