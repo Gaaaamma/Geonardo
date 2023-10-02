5ca62e51bb9a8731b764f8f059000f5d16e719b4
Gaaaamma <ytching552022@gmail.com>
2023-10-02 12:56:07 +0800

    Add: read data from serial and echo

--- leoMapleController.ino
+++ leoMapleController.ino
@@ -1,9 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available()) {
+    while (Serial.available()) {
+      int c = Serial.read();
+      Serial.print(char(c));
+    }
+  }
 }
+
+/*
+
+*/
